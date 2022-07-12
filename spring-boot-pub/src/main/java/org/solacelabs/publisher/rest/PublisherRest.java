d0038ec33ab32a87fd1d5ed1d928ef06df1c1d83
ruffp <prx.list@gmail.com>
2022-07-12 23:33:29 +0200

    Added shovel in rabbit docker image

--- spring-boot-pub/src/main/java/org/solacelabs/publisher/rest/PublisherRest.java
+++ spring-boot-pub/src/main/java/org/solacelabs/publisher/rest/PublisherRest.java
@@ -3,7 +3,6 @@ package org.solacelabs.publisher.rest;
 import org.solacelabs.publisher.service.PublisherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
